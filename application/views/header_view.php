37739441c8c47b8dbc53ec068d6ddd15ba5cc6e5
Ahmad Nur Fahmi <Ahmadnurdante@gmail.com>
2022-12-11 08:46:48 +0700

    final

--- application/views/header_view.php
+++ application/views/header_view.php
@@ -5,7 +5,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title><?php echo $title_web; ?> | Management Information System </title>
+  <title><?php echo $title_web; ?> | Sistem Informasi Perpustakaan Codekop </title>
   <!-- Tell the browser to be responsive to screen width -->
 
 
@@ -15,13 +15,30 @@
   <!-- Font Awesome -->
   <link rel="stylesheet" href="<?php echo base_url(); ?>assets_style/assets/bower_components/font-awesome/css/font-awesome.min.css">
 
+
+  <!-- Select2 -->
+  <link rel="stylesheet" href="<?php echo base_url(); ?>assets_style/assets/bower_components/select2/dist/css/select2.min.css">
+
   <!-- Ionicons -->
   <link rel="stylesheet" href="<?php echo base_url(); ?>assets_style/assets/bower_components/Ionicons/css/ionicons.min.css">
+  <!-- Theme style -->
+
+  <link href="<?php echo base_url(); ?>assets_style/assets/plugins/summernote/summernote-lite.css" rel="stylesheet">
 
   <link rel="stylesheet" href="<?php echo base_url(); ?>assets_style/assets/dist/css/AdminLTE.css">
-
   <link rel="stylesheet" href="<?php echo base_url(); ?>assets_style/assets/dist/css/responsive.css">
 
+  <!-- Bootstrap time Picker -->
+  <link rel="stylesheet" href="<?php echo base_url(); ?>assets_style/assets/plugins/timepicker/bootstrap-timepicker.min.css">
+  <!-- bootstrap datepicker -->
+  <link rel="stylesheet" href="<?php echo base_url(); ?>assets_style/assets/bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css">
+  <!-- DataTables -->
+  <link rel="stylesheet" href="<?php echo base_url(); ?>assets_style/assets/bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css">
+  <!-- AdminLTE Skins. Choose a skin from the css/skins
+       folder instead of downloading all of them to reduce the load. -->
+  <link rel="stylesheet" href="<?php echo base_url(); ?>assets_style/assets/dist/css/skins/_all-skins.min.css">
+
+  <link rel="stylesheet" href="<?php echo base_url(); ?>assets_style/assets/plugins/pace/pace.min.css">
   <!-- jQuery 3 -->
   <script src="<?php echo base_url(); ?>assets_style/assets/bower_components/jquery/dist/jquery.min.js"></script>
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
@@ -32,37 +49,7 @@
   <![endif]-->
   <!-- Google Font -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
-
-  <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>GIS Leaflet : <?= $title; ?></title>
-  <!-- BOOTSTRAP STYLES-->
-  <link href="<?= base_url('template/assets/css/bootstrap.css'); ?>" rel="stylesheet" />
-  <!-- FONTAWESOME STYLES-->
-  <link href="<?= base_url('template/assets/css/font-awesome.css'); ?>" rel="stylesheet" />
-  <!-- CUSTOM STYLES-->
-  <link href="<?= base_url('template/assets/css/custom.css'); ?>" rel="stylesheet" />
-  <!-- GOOGLE FONTS-->
-  <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
-  <!-- LEAFLET -->
-  <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
-  <link rel="stylesheet" href="https://unpkg.com/leaflet-routing-machine@latest/dist/leaflet-routing-machine.css" />
-  <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
-  <script src="https://unpkg.com/leaflet-routing-machine@latest/dist/leaflet-routing-machine.js"></script>
-
-  <!-- SCRIPTS -AT THE BOTOM TO REDUCE THE LOAD TIME-->
-  <!-- JQUERY SCRIPTS -->
-  <script src="<?= base_url('template/assets/js/jquery-1.10.2.js'); ?>"></script>
-  <!-- BOOTSTRAP SCRIPTS -->
-  <script src="<?= base_url('template/assets/js/bootstrap.min.js'); ?>"></script>
-  <!-- METISMENU SCRIPTS -->
-  <script src="<?= base_url('template/assets/js/jquery.metisMenu.js'); ?>"></script>
-  <!-- CUSTOM SCRIPTS -->
-  <script src="<?= base_url('template/assets/js/custom.js'); ?>"></script>
-
-  <!-- LEAFLET CONTROL SEARCH -->
-  <link rel="stylesheet" href="<?= base_url('leaflet-search/src/leaflet-search.css'); ?>" />
-  <script src="<?= base_url('leaflet-search/src/leaflet-search.js'); ?>"></script>
+  <!-- offline -->
   <script type="text/javascript">
     $(document).ajaxStart(function() {
       Pace.restart();
@@ -70,16 +57,16 @@
   </script>
 </head>
 
-<body class="hold-transition sidebar-mini">
+<body class="hold-transition skin-blue-light sidebar-mini">
   <div class="wrapper">
     <header class="main-header">
 
       <!-- Logo -->
       <a href="<?php echo base_url('index.php/dashboard'); ?>" class="logo">
         <!-- mini logo for sidebar mini 50x50 pixels -->
-        <span class="logo-mini"><b>R</b>I</b>D</span>
+        <span class="logo-mini"><b>R</b>I</span>
         <!-- logo for regular state and mobile devices -->
-        <span class="logo-lg">RESIDFOM</span>
+        <span class="logo-lg">Residfom</span>
       </a>
       <!-- Header Navbar: style can be found in header.less -->
       <nav class="navbar navbar-static-top">
